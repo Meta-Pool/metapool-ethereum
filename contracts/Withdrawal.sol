6e5632fa4d8c038ff330e6964d0b79f66996e84a
leanonchain <leandroezeleguizamon@gmail.com>
2023-04-24 22:57:19 -0300

    Add comments to Withdrawal for documentation and minor changes

--- contracts/Withdrawal.sol
+++ contracts/Withdrawal.sol
@@ -12,37 +12,46 @@ struct withdrawRequest {
     uint unlockEpoch;
 }
 
+/// @title Manage withdrawals from validators to users
+/// @notice Receive request for withdrawals from Staking and allow users to complete the withdrawals once the epoch is reached
+/// @dev As the disassemble of validators is delayed, this contract manage the pending withdraw from users to allow the to complet it once his unlockEpoch is reached and if the contract has enough ETH
+/// The epochs are of one week
 contract Withdrawal is OwnableUpgradeable {
     using AddressUpgradeable for address payable;
     using SafeERC20Upgradeable for IERC20Upgradeable;
 
     address payable public mpETH;
     uint public totalPendingWithdraw;
-    uint private startTimestamp;
+    uint public startTimestamp;
     mapping(address => withdrawRequest) public pendingWithdraws;
 
     event RequestWithdraw(address indexed user, uint amount, uint unlockEpoch);
     event CompleteWithdraw(address indexed user, uint amount, uint unlockEpoch);
 
+    modifier onlyStaking() {
+        require(msg.sender == mpETH, "Caller not Staking");
+        _;
+    }
+
     receive() external payable {}
 
     function initialize(address payable _mpETH) external initializer {
+        __Ownable_init();
         startTimestamp = block.timestamp;
         mpETH = _mpETH;
-        __Ownable_init();
         transferOwnership(_mpETH);
     }
 
+    /// @return epoch Returns the current epoch
     function getEpoch() public view returns (uint epoch) {
         return (block.timestamp - startTimestamp) / 7 days;
     }
 
     /// @notice Queue ETH withdrawal
-    /// @dev Multiples withdrawals are accumulative, but will restart the timestamp unlock
+    /// @dev Multiples withdrawals are accumulative, but will restart the epoch unlock
     /// @param _amountOut ETH amount to withdraw
     /// @param _user Owner of the withdrawal
-    function requestWithdraw(uint _amountOut, address _user) external {
-        require(msg.sender == mpETH, "Caller is not staking contract");
+    function requestWithdraw(uint _amountOut, address _user) external onlyStaking {
         uint unlockEpoch = getEpoch() + 1;
         pendingWithdraws[_user].amount += _amountOut;
         pendingWithdraws[_user].unlockEpoch = unlockEpoch;
@@ -58,8 +67,8 @@ contract Withdrawal is OwnableUpgradeable {
             "Withdrawal delay not reached"
         );
         require(_withdrawR.amount > 0, "Nothing to withdraw");
-        pendingWithdraws[msg.sender] = withdrawRequest(0, 0);
         totalPendingWithdraw -= _withdrawR.amount;
+        delete pendingWithdraws[msg.sender];
         payable(msg.sender).sendValue(_withdrawR.amount);
         emit CompleteWithdraw(
             msg.sender,
@@ -68,13 +77,14 @@ contract Withdrawal is OwnableUpgradeable {
         );
     }
 
-    /// @notice Send ETH balance to Staking
-    /// @dev Send ETH to Staking. This shouldn't mint new mpETH
+    /// @notice Send ETH _amount to Staking
+    /// @dev As the validators are always fully disassembled, the contract can have more ETH than the needed for withdrawals. So the Staking can take this ETH and send it again to validators. This shouldn't mint new mpETH
     function getEthForValidator(uint _amount) external onlyOwner {
         require(_amount <= ethRemaining(), "Not enough ETH to stake");
         mpETH.sendValue(_amount);
     }
 
+    /// @notice Returns the ETH not assigned to any withdrawal
     function ethRemaining() public view returns (uint) {
         return
             (address(this).balance > totalPendingWithdraw)
