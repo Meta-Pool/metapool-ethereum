59decc4ec58d46514f2e80f464766c11f7f22887
leanonchain <leandroezeleguizamon@gmail.com>
2023-05-13 13:31:18 -0300

    Withdrawal test catch customErrors and check mpETH price

--- test/Withdrawal.ts
+++ test/Withdrawal.ts
@@ -73,32 +73,43 @@ describe("Withdrawal", function () {
     var staking: Contract,
       withdrawal: Contract,
       owner: SignerWithAddress,
-      user: SignerWithAddress;
+      user: SignerWithAddress,
+      updater: SignerWithAddress,
+      activator: SignerWithAddress;
 
     it("Deposit ETH for mpETH", async () => {
-      ({ user, owner, staking, withdrawal } = await loadFixture(deployTest));
+      ({ user, owner, staking, withdrawal, updater, activator } = await loadFixture(deployTest));
       const depositAmount = toEthers(32);
       await staking.connect(user).depositETH(user.address, { value: depositAmount });
+      await staking.connect(activator).pushToBeacon([getNextValidator()], 0, 0);
+      await staking.connect(updater).updateNodesBalance(toEthers(32.032));
+      const timelock = await staking.UPDATE_BALANCE_TIMELOCK();
+      await time.increase(timelock);
+      await staking.connect(updater).updateNodesBalance(toEthers(32.032));
+      expect(await staking.estimatedRewardsPerSecond()).to.eq(0);
     });
 
     it("Request withdraw", async () => {
+      const mpETHPrice = await staking.convertToAssets(toEthers(1));
       const userBalance = await staking.balanceOf(user.address);
       await staking.connect(user).approve(staking.address, userBalance);
-      await staking.connect(user).withdraw(userBalance, user.address, user.address);
+      const estimatedRewardsPerSecond = await staking.estimatedRewardsPerSecond();
+      await staking.connect(user).redeem(userBalance, user.address, user.address);
       const unlockEpoch = BigNumber.from(
         (await withdrawal.pendingWithdraws(user.address)).unlockEpoch
       );
       expect(await staking.balanceOf(user.address)).to.eq(0);
-      expect((await withdrawal.pendingWithdraws(user.address)).amount).to.eq(userBalance);
+      expect((await withdrawal.pendingWithdraws(user.address)).amount).to.gt(userBalance.add(estimatedRewardsPerSecond));
       expect((await withdrawal.pendingWithdraws(user.address)).unlockEpoch).to.eq(
         unlockEpoch
       );
-      expect(await withdrawal.totalPendingWithdraw()).to.eq(userBalance);
+      // Almost equal for rounded values
+      expect((await staking.convertToAssets(toEthers(1))).toString().substring(1, 8)).to.eq(mpETHPrice.toString().substring(1, 8))
     });
 
     it("Complete withdraw must revert before unlock time", async () => {
-      await expect(withdrawal.connect(user).completeWithdraw()).to.be.revertedWith(
-        "Withdrawal delay not reached"
+      await expect(withdrawal.connect(user).completeWithdraw()).to.be.revertedWithCustomError(withdrawal,
+        "EpochNotReached"
       );
     });
 
@@ -126,51 +137,60 @@ describe("Withdrawal", function () {
       withdrawal: Contract,
       owner: SignerWithAddress,
       user: SignerWithAddress,
-      activator: SignerWithAddress;
+      activator: SignerWithAddress,
+      updater: SignerWithAddress;
+    const depositAmount = toEthers(32),
+      withdrawalAmount = BigNumber.from(depositAmount).div(2);
 
     it("Revert stake with 0 ETH balance", async () => {
-      ({ user, owner, staking, withdrawal, activator } = await loadFixture(deployTest));
+      ({ user, owner, staking, withdrawal, activator, updater } = await loadFixture(deployTest));
 
       await expect(
         staking.connect(activator).pushToBeacon([getNextValidator()], 0, toEthers(32))
-      ).to.be.revertedWith("Not enough ETH to stake");
+      ).to.be.revertedWithCustomError(withdrawal, "NotEnoughETHtoStake");
     });
 
     it("Revert staking with pending withdraw gt balance", async () => {
-      const value = toEthers(32);
-      await staking.connect(user).depositETH(user.address, { value });
-      await staking.connect(user).approve(staking.address, value);
-      await staking.connect(user).withdraw(value, user.address, user.address);
-      expect(await withdrawal.totalPendingWithdraw()).to.eq(value);
+      await staking.connect(user).depositETH(user.address, { value: depositAmount });
+      await staking.connect(user).approve(staking.address, withdrawalAmount);
+      await staking.connect(user).withdraw(withdrawalAmount, user.address, user.address);
+      expect(await withdrawal.totalPendingWithdraw()).to.eq(withdrawalAmount);
 
       await owner.sendTransaction({
         to: withdrawal.address,
-        value,
+        value: withdrawalAmount,
       });
 
       await expect(
         staking.connect(activator).pushToBeacon([getNextValidator()], 0, toEthers(32))
-      ).to.be.revertedWith("Not enough ETH to stake");
+      ).to.be.revertedWithCustomError(withdrawal, "NotEnoughETHtoStake");
     });
 
     it("Stake remaining ETH", async () => {
-      ({ user, owner, staking, withdrawal } = await loadFixture(deployTest));
+      await staking.connect(user).depositETH(user.address, { value: toEthers(32) });
+      await staking.connect(activator).pushToBeacon([getNextValidator()], 0, 0)
 
-      const value = toEthers(32);
+
       await owner.sendTransaction({
         to: withdrawal.address,
-        value,
+        value: withdrawalAmount,
       });
-
-      expect(await provider.getBalance(withdrawal.address)).eq(value);
-      expect(await provider.getBalance(staking.address)).eq(0);
-      expect(await staking.stakingBalance()).eq(0);
+      expect(await provider.getBalance(withdrawal.address)).eq(depositAmount);
+      expect(await provider.getBalance(staking.address)).eq(depositAmount);
+      expect(await staking.stakingBalance()).eq(depositAmount);
+      await staking.connect(updater).updateNodesBalance(toEthers(16.032));
+      const timelock = await staking.UPDATE_BALANCE_TIMELOCK();
+      await time.increase(timelock);
+      await staking.connect(updater).updateNodesBalance(toEthers(16.032));
+      const mpETHPrice = await staking.convertToAssets(toEthers(1));
+      expect(await staking.estimatedRewardsPerSecond()).to.eq(0);
       await staking
         .connect(activator)
-        .pushToBeacon([getNextValidator()], 0, toEthers(32));
-      expect(await provider.getBalance(withdrawal.address)).eq(0);
-      expect(await provider.getBalance(staking.address)).eq(0);
-      expect(await staking.stakingBalance()).eq(0);
+        .pushToBeacon([getNextValidator()], 0, withdrawalAmount);
+      expect(await provider.getBalance(withdrawal.address)).eq(withdrawalAmount);
+      expect(await provider.getBalance(staking.address)).eq(withdrawalAmount);
+      expect(await staking.stakingBalance()).eq(withdrawalAmount);
+      expect((await staking.convertToAssets(toEthers(1))).toString()).to.eq(mpETHPrice.toString())
     });
   });
 });
